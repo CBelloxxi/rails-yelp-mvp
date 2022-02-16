61f8a77e5eb940cd832233a16dec6a179546b006
Christopher Bellocci <christopher.bellocci@hotmail.com>
2022-02-16 14:43:21 +0000

    added validations

--- db/migrate/20220216125601_create_reviews.rb
+++ db/migrate/20220216125601_create_reviews.rb
@@ -2,7 +2,8 @@ class CreateReviews < ActiveRecord::Migration[6.1]
   def change
     create_table :reviews do |t|
       t.integer :rating
-      t.text :content
+      t.string :content
+      t.references :restaurant, null: false, foreign_key: true
 
       t.timestamps
     end
