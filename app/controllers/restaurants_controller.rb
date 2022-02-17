b3c62f40a267122c9546890be1be41d3ac04f9ae
Christopher Bellocci <christopher.bellocci@hotmail.com>
2022-02-17 11:52:05 +0000

    last changes from yesterday, didn't manage to finish

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,31 @@
 class RestaurantsController < ApplicationController
+  before_action :find_restaurant, only: [:show]
+
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show
+  end
+
+  def create
+    @restaurant = Restaurant.new(set_restaurant)
+    @restaurant.save
+    redirect_to restaurants_path(@restaurant)
+  end
+
+  private
+
+  def find_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def set_restaurant
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
+  end
+
+  def task_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
+  end
+
 end
